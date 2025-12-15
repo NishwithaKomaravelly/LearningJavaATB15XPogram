d5245252ecebf0e53544f54953b8f5d5602dfffa
nishwitha <nishwitha.93@gmail.com>
2025-12-14 20:24:42 -0800

    My first Java program

---
+++ ex_01_Java_Basics/Lab002_Keywords_Identifiers.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab002_Keywords_Identifiers {
+    public static void main(String[] args) {
+        System.out.println("We have 54 reserve Keywords");
+        System.out.println("+17 contextual Keywords");
+        System.out.println("-3 not reserve Keywords");
+        System.out.println("so in total we have 68");
+        System.out.println("Identifiers are nothing but class/package name");
+        System.out.println("class name can have - and $ and follow camel case starting with capitals");
+    }
+}
